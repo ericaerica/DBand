cd7f8a8abed05c19c561d259867c0d4c883edf63
Erica_2 <Erica_2@Erica-PC>
2014-03-22 17:06:21 +0100

    update

--- DBand/src/insertionGUI/BandInsertion.java
+++ DBand/src/insertionGUI/BandInsertion.java
@@ -14,6 +14,7 @@ import javax.swing.JComboBox;
 import javax.swing.JButton;
 import javax.swing.JScrollPane;
 import java.awt.GridLayout;
+import javax.swing.JTextArea;
 
 public class BandInsertion extends JFrame {
 
@@ -21,12 +22,6 @@ public class BandInsertion extends JFrame {
 	private JTextField textField;
 	private JTextField textField_2;
 	private JTextField txtGenregenregenre;
-	private JTextField txtNameSurname;
-	private JTextField txtBirthCountry;
-	private JTextField txtSurname;
-	private JTextField txtStageName;
-	private JTextField txtDdmmyyyy;
-	private JTextField txtActiveinactive;
 
 	/**
 	 * Launch the application.
@@ -59,43 +54,43 @@ public class BandInsertion extends JFrame {
 		JLabel lblCompleteTheFields = new JLabel("Complete the fields to insert a new band");
 		lblCompleteTheFields.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCompleteTheFields.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblCompleteTheFields.setBounds(86, 44, 395, 32);
+		lblCompleteTheFields.setBounds(0, 44, 734, 32);
 		contentPane.add(lblCompleteTheFields);
 		
 		JLabel lblNameOfThe = new JLabel("Name of the band:");
 		lblNameOfThe.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNameOfThe.setBounds(103, 107, 144, 22);
+		lblNameOfThe.setBounds(186, 106, 144, 22);
 		contentPane.add(lblNameOfThe);
 		
 		textField = new JTextField();
 		textField.setColumns(10);
-		textField.setBounds(298, 108, 175, 22);
+		textField.setBounds(381, 107, 175, 22);
 		contentPane.add(textField);
 		
 		JLabel lblCountryOfOrigin = new JLabel("Country of origin:");
 		lblCountryOfOrigin.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblCountryOfOrigin.setBounds(102, 142, 118, 22);
+		lblCountryOfOrigin.setBounds(185, 141, 118, 22);
 		contentPane.add(lblCountryOfOrigin);
 		
 		JLabel lblGenre = new JLabel("Genre:");
 		lblGenre.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblGenre.setBounds(103, 179, 156, 16);
+		lblGenre.setBounds(186, 178, 156, 16);
 		contentPane.add(lblGenre);
 		
-		JLabel lblMembers = new JLabel("Members (name, surname,\r\nstage name, country of birth,\r\ndate of birth):");
+		JLabel lblMembers = new JLabel("Members (name;surname;stage name;country of birth;date of birth;active/inactive;instrument):");
 		lblMembers.setVerticalAlignment(SwingConstants.TOP);
 		lblMembers.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblMembers.setBounds(103, 233, 144, 85);
+		lblMembers.setBounds(57, 224, 640, 32);
 		contentPane.add(lblMembers);
 		
 		textField_2 = new JTextField();
-		textField_2.setBounds(298, 142, 175, 22);
+		textField_2.setBounds(381, 141, 175, 22);
 		contentPane.add(textField_2);
 		textField_2.setColumns(10);
 		
 		txtGenregenregenre = new JTextField();
 		txtGenregenregenre.setText("genre1;genre2;genre3");
-		txtGenregenregenre.setBounds(298, 177, 175, 22);
+		txtGenregenregenre.setBounds(381, 176, 175, 22);
 		contentPane.add(txtGenregenregenre);
 		txtGenregenregenre.setColumns(10);
 		
@@ -105,55 +100,11 @@ public class BandInsertion extends JFrame {
 		contentPane.add(button);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(298, 214, 385, 295);
+		scrollPane.setBounds(100, 269, 541, 235);
 		contentPane.add(scrollPane);
 		
-		JPanel panel = new JPanel();
-		scrollPane.setViewportView(panel);
-		panel.setLayout(new GridLayout(0, 1, 0, 0));
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setLayout(null);
-		panel.add(panel_1);
-		
-		txtNameSurname = new JTextField();
-		txtNameSurname.setText("name");
-		txtNameSurname.setColumns(10);
-		txtNameSurname.setBounds(0, 13, 70, 22);
-		panel_1.add(txtNameSurname);
-		
-		txtBirthCountry = new JTextField();
-		txtBirthCountry.setText("birth country");
-		txtBirthCountry.setColumns(10);
-		txtBirthCountry.setBounds(287, 13, 96, 22);
-		panel_1.add(txtBirthCountry);
-		
-		txtSurname = new JTextField();
-		txtSurname.setText("surname");
-		txtSurname.setBounds(88, 13, 80, 22);
-		panel_1.add(txtSurname);
-		txtSurname.setColumns(10);
-		
-		txtStageName = new JTextField();
-		txtStageName.setText("stage name");
-		txtStageName.setBounds(188, 13, 87, 22);
-		panel_1.add(txtStageName);
-		txtStageName.setColumns(10);
-		
-		txtDdmmyyyy = new JTextField();
-		txtDdmmyyyy.setText("dd/mm/yyyy");
-		txtDdmmyyyy.setBounds(0, 48, 116, 22);
-		panel_1.add(txtDdmmyyyy);
-		txtDdmmyyyy.setColumns(10);
-		
-		txtActiveinactive = new JTextField();
-		txtActiveinactive.setText("active/inactive");
-		txtActiveinactive.setBounds(131, 48, 116, 22);
-		panel_1.add(txtActiveinactive);
-		txtActiveinactive.setColumns(10);
-		
-		JButton button_1 = new JButton("+");
-		button_1.setBounds(693, 230, 41, 25);
-		contentPane.add(button_1);
+		JTextArea txtrPleasePressEnter = new JTextArea();
+		scrollPane.setViewportView(txtrPleasePressEnter);
+		txtrPleasePressEnter.setText("please press enter key after the info about every  member");
 	}
 }
--- DBand/src/mainGUI/Update.java
+++ DBand/src/mainGUI/Update.java
@@ -9,10 +9,16 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Update extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -35,33 +41,58 @@ public class Update extends JFrame {
 	 */
 	public Update() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 170, 252);
+		setBounds(100, 100, 578, 474);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblInsertNewItem = new JLabel("Update Item");
+		JLabel lblInsertNewItem = new JLabel("Please select the information you want to update");
 		lblInsertNewItem.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		lblInsertNewItem.setHorizontalAlignment(SwingConstants.CENTER);
-		lblInsertNewItem.setBounds(10, 11, 134, 44);
+		lblInsertNewItem.setBounds(0, 11, 560, 44);
 		contentPane.add(lblInsertNewItem);
 		
-		JButton btnAlbum = new JButton("Album");
-		btnAlbum.setBounds(30, 62, 89, 23);
-		contentPane.add(btnAlbum);
+		JLabel lblChooseATable = new JLabel("Choose a table:");
+		lblChooseATable.setBounds(119, 81, 104, 16);
+		contentPane.add(lblChooseATable);
 		
-		JButton btnBand = new JButton("Band");
-		btnBand.setBounds(30, 96, 89, 23);
-		contentPane.add(btnBand);
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"", "band", "musician", "genre", "album", "song", "concert", "award"}));
+		comboBox.setBounds(256, 78, 195, 22);
+		contentPane.add(comboBox);
 		
-		JButton btnConcert = new JButton("Concert");
-		btnConcert.setBounds(30, 130, 89, 23);
-		contentPane.add(btnConcert);
+		JLabel lblChooseAField = new JLabel("Choose an item:");
+		lblChooseAField.setBounds(119, 126, 96, 16);
+		contentPane.add(lblChooseAField);
 		
-		JButton btnAward = new JButton("Award");
-		btnAward.setBounds(30, 164, 89, 23);
-		contentPane.add(btnAward);
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setBounds(256, 123, 195, 22);
+		contentPane.add(comboBox_1);
+		
+		JLabel lblNewLabel = new JLabel("Choose a field:");
+		lblNewLabel.setBounds(119, 176, 96, 16);
+		contentPane.add(lblNewLabel);
+		
+		JComboBox comboBox_2 = new JComboBox();
+		comboBox_2.setBounds(256, 173, 195, 22);
+		contentPane.add(comboBox_2);
+		
+		JLabel lblReplaceWith = new JLabel("Replace with:");
+		lblReplaceWith.setBounds(119, 225, 96, 16);
+		contentPane.add(lblReplaceWith);
+		
+		textField = new JTextField();
+		textField.setBounds(256, 222, 195, 22);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnOk = new JButton("OK!");
+		btnOk.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnOk.setBounds(463, 403, 97, 25);
+		contentPane.add(btnOk);
 	}
-
 }
