67828f9945734a800e13df6142c1cf7ba92adc00
Erica_2 <Erica_2@Erica-PC>
2014-03-22 01:02:49 +0100

    ciao bbbbelli (commit di album e band)

---
+++ DBand/src/gui/AlbumInsertion.java
@@ -0,0 +1,148 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.GridLayout;
+import javax.swing.JScrollPane;
+
+public class AlbumInsertion extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField txtYyyy;
+	private JTextField txtName;
+	private JTextField txtCountry;
+	private JTextField textField_1;
+	private JTextField textField_2;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AlbumInsertion frame = new AlbumInsertion();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AlbumInsertion() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 637, 523);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(293, 135, 175, 22);
+		comboBox.setToolTipText("");
+		contentPane.add(comboBox);
+		
+		JLabel lblInsertANew = new JLabel("Complete the fields to insert a new album");
+		lblInsertANew.setBounds(81, 37, 395, 32);
+		lblInsertANew.setHorizontalAlignment(SwingConstants.CENTER);
+		lblInsertANew.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		contentPane.add(lblInsertANew);
+		
+		JLabel lblName = new JLabel("Name of the album:");
+		lblName.setBounds(98, 100, 144, 22);
+		lblName.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		contentPane.add(lblName);
+		
+		textField = new JTextField();
+		textField.setBounds(293, 101, 175, 22);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblBand = new JLabel("Band name:");
+		lblBand.setBounds(98, 137, 118, 16);
+		lblBand.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		contentPane.add(lblBand);
+		
+		JLabel lblYear = new JLabel("Year of production:");
+		lblYear.setBounds(98, 172, 156, 16);
+		lblYear.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		contentPane.add(lblYear);
+		
+		txtYyyy = new JTextField();
+		txtYyyy.setBounds(293, 170, 175, 22);
+		txtYyyy.setText("yyyy");
+		txtYyyy.setToolTipText("");
+		contentPane.add(txtYyyy);
+		txtYyyy.setColumns(10);
+		
+		JLabel lblRecordLabel = new JLabel("Record Label:");
+		lblRecordLabel.setBounds(98, 207, 144, 16);
+		lblRecordLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		contentPane.add(lblRecordLabel);
+		
+		txtName = new JTextField();
+		txtName.setBounds(293, 205, 118, 22);
+		txtName.setText("name");
+		contentPane.add(txtName);
+		txtName.setColumns(10);
+		
+		JLabel lblSongs = new JLabel("Songs:");
+		lblSongs.setBounds(98, 256, 56, 22);
+		lblSongs.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		contentPane.add(lblSongs);
+		
+		txtCountry = new JTextField();
+		txtCountry.setBounds(423, 205, 116, 22);
+		txtCountry.setText("country");
+		contentPane.add(txtCountry);
+		txtCountry.setColumns(10);
+		
+		JButton btnInsert = new JButton("Insert!");
+		btnInsert.setBounds(522, 452, 97, 25);
+		btnInsert.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		contentPane.add(btnInsert);
+		
+		JButton btnMore = new JButton("+");
+		btnMore.setBounds(566, 256, 41, 25);
+		contentPane.add(btnMore);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(293, 240, 270, 199);
+		contentPane.add(scrollPane);
+		
+		JPanel panel = new JPanel();
+		scrollPane.setRowHeaderView(panel);
+		panel.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JPanel panel_1 = new JPanel();
+		scrollPane.setViewportView(panel_1);
+		panel_1.setLayout(null);
+		
+		textField_1 = new JTextField();
+		textField_1.setText("title");
+		textField_1.setColumns(10);
+		textField_1.setBounds(0, 13, 127, 22);
+		panel_1.add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setText("hh:mm:ss");
+		textField_2.setColumns(10);
+		textField_2.setBounds(139, 13, 129, 22);
+		panel_1.add(textField_2);
+	}
+}
---
+++ DBand/src/gui/BandInsertion.java
@@ -0,0 +1,151 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import java.awt.GridLayout;
+
+public class BandInsertion extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField txtNameSurname;
+	private JTextField txtBirthCountry;
+	private JTextField txtSurname;
+	private JTextField txtStageName;
+	private JTextField txtDdmmyyyy;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					BandInsertion frame = new BandInsertion();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public BandInsertion() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 764, 593);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblCompleteTheFields = new JLabel("Complete the fields to insert a new band");
+		lblCompleteTheFields.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCompleteTheFields.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblCompleteTheFields.setBounds(86, 44, 395, 32);
+		contentPane.add(lblCompleteTheFields);
+		
+		JLabel lblNameOfThe = new JLabel("Name of the band:");
+		lblNameOfThe.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNameOfThe.setBounds(103, 107, 144, 22);
+		contentPane.add(lblNameOfThe);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		textField.setBounds(298, 108, 175, 22);
+		contentPane.add(textField);
+		
+		JLabel lblCountryOfOrigin = new JLabel("Country of origin:");
+		lblCountryOfOrigin.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblCountryOfOrigin.setBounds(102, 142, 118, 22);
+		contentPane.add(lblCountryOfOrigin);
+		
+		JLabel lblGenre = new JLabel("Genre:");
+		lblGenre.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblGenre.setBounds(103, 179, 156, 16);
+		contentPane.add(lblGenre);
+		
+		JLabel lblMembers = new JLabel("Members (name, surname,\r\nstage name, country of birth,\r\ndate of birth):");
+		lblMembers.setVerticalAlignment(SwingConstants.TOP);
+		lblMembers.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblMembers.setBounds(103, 233, 144, 85);
+		contentPane.add(lblMembers);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(298, 142, 175, 22);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(298, 177, 175, 22);
+		contentPane.add(textField_3);
+		textField_3.setColumns(10);
+		
+		JButton button = new JButton("Insert!");
+		button.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		button.setBounds(649, 522, 97, 25);
+		contentPane.add(button);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(298, 214, 385, 295);
+		contentPane.add(scrollPane);
+		
+		JPanel panel = new JPanel();
+		scrollPane.setViewportView(panel);
+		panel.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setLayout(null);
+		panel.add(panel_1);
+		
+		txtNameSurname = new JTextField();
+		txtNameSurname.setText("name");
+		txtNameSurname.setColumns(10);
+		txtNameSurname.setBounds(0, 13, 70, 22);
+		panel_1.add(txtNameSurname);
+		
+		txtBirthCountry = new JTextField();
+		txtBirthCountry.setText("birth country");
+		txtBirthCountry.setColumns(10);
+		txtBirthCountry.setBounds(287, 13, 96, 22);
+		panel_1.add(txtBirthCountry);
+		
+		txtSurname = new JTextField();
+		txtSurname.setText("surname");
+		txtSurname.setBounds(88, 13, 80, 22);
+		panel_1.add(txtSurname);
+		txtSurname.setColumns(10);
+		
+		txtStageName = new JTextField();
+		txtStageName.setText("stage name");
+		txtStageName.setBounds(188, 13, 87, 22);
+		panel_1.add(txtStageName);
+		txtStageName.setColumns(10);
+		
+		txtDdmmyyyy = new JTextField();
+		txtDdmmyyyy.setText("dd/mm/yyyy");
+		txtDdmmyyyy.setBounds(0, 48, 116, 22);
+		panel_1.add(txtDdmmyyyy);
+		txtDdmmyyyy.setColumns(10);
+		
+		JButton button_1 = new JButton("+");
+		button_1.setBounds(693, 230, 41, 25);
+		contentPane.add(button_1);
+	}
+
+}
